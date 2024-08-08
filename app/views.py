841e2e548ef46b50a01ec51a741ca35b7f10b309
hiewbt <hieubt.bk66@gmail.com>
2024-08-08 14:22:21 +0700

    Design static pages

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+    context = {
+    }
+    return render(request, 'index.html', context=context)
